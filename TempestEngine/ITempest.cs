584369e11f68a06e0de13110c4b6fb6167fddc44
SilentSiren-S2 <91809999+SilentSiren-S2@users.noreply.github.com>
2024-03-02 14:17:35 +0200

    engine_basics

---
+++ TempestEngine/ITempest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempestEngine
+{
+    internal interface ITempest
+    {
+    }
+
+    public interface IEntity { }
+    public interface IMovable { }
+    public interface IKickable { }
+    public interface ILevel { }
+}
